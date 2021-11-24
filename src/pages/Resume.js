d49f67e7661f0db5cc1a433b968920034fece6f6
DuyNguyen19993010 <yuddayama@gmail.com>
2021-11-24 20:58:31 +0000

    Release 1.01

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,17 +2,18 @@
 import '../css/Resume.css';
 //Image
 import page_1 from '../resources/Cv_page1.jpg'
-import page_2 from '../resources/Cv_page2.jpg'
+// import page_2 from '../resources/Cv_page2.jpg'
 import { useState } from 'react';
 const Resume=()=> {
-    const [page_no_1,setPage] = useState(true)
-    const changePage = ()=>{
-        setPage(!page_no_1)
-    }
+    // const [page_no_1,setPage] = useState(true)
+    // const changePage = ()=>{
+    //     setPage(!page_no_1)
+    // }
     return (
     <div className="Resume">
        <div className="content">
-           { page_no_1== true?(<img onClick={changePage} src= {page_1}/>):(<img onClick={changePage} src= {page_2}/>)}
+           {/* { page_no_1== true?(<img onClick={changePage} src= {page_1}/>):(<img onClick={changePage} src= {page_2}/>)} */}
+           <img src= {page_1}/>
        </div>
     </div>
   );
