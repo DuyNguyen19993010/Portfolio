1737fc6b194a64f3fe669c4a64252e5890293191
yuddayama <yuddayama@gmail.com>
2023-03-20 23:41:41 +0000

    Updated scalability

--- src/Home page/App.js
+++ src/Home page/App.js
@@ -1,10 +1,12 @@
 import '../CSS/App.css';
+import '../fonts/post-no-bills/postnobillscolombo-extrabold.ttf'
 import NavBar from '../Components/NavBar';
 import UCLBanner from '../SVG/UCLBanner.svg'
 import QueenMaryBanner from '../SVG/QueenMaryBanner.svg'
 import LanguageIcon from '../SVG/35-capsule.svg'
 import FrontEndIcon from '../SVG/45-astronaut.svg'
 import BackEndIcon from '../SVG/23-sputnik.svg'
+import Shuttle from '../SVG/31-shuttle.svg'
 import spaceship from "../SVG/toy-space-ship-svgrepo-com.svg"
 import { useState } from 'react';
 import Skillbox from "../Components/SkillBox"
@@ -18,8 +20,8 @@ function App() {
       <NavBar/>
       <div className='tabs' id='Home'>
         {/* Logo image */}
-        <img id="logo-image" src = "/Resources/Images/Logo.png" alt="Could not load Logo" />
-        
+        {/* <img id="logo-image" src = "/Resources/Images/Logo.png" alt="Could not load Logo" /> */}
+        <img id="background-image" src="/Resources/Images/FirstPageBG.jpg" alt="Could not load background"/>
         {/* Profile wrapper */}
         <div id="profile-wrapper">
           <div id="border-1"></div>
@@ -28,30 +30,27 @@ function App() {
         </div>
 
         {/* Intro wrapper */}
-        <div id="intro-wrapper">
-          <h1 id='intro-1'>
-          TAKE A LOOK AROUND
-          </h1>
-          <h1 id='intro-2'>
-          ENJOY THE RIDE
-          </h1>
-          <div id="divider-2">
-
+          <div id="intro-wrapper">
+            <h1 id='intro-1'>
+            TAKE A LOOK AROUND
+            </h1>
+            <h1 id='intro-2'>
+            ENJOY THE RIDE
+            </h1>
+            <div id="divider-2">
           </div>
         </div>
 
         {/* Background image */}
-        <img id="background-image" src="/Resources/Images/FirstPageBG.jpg" alt="Could not load background"/>
 
       </div>
       
       {/* Tab 2 */}
       <div className='tabs' id='Tab2'>
-        <div id="intro-text-wrapper">
+          <div id="intro-text-wrapper">
             {/* Achievement wrapper */}
             <div id="achievement-heading-wrapper">
-              <h2 id='Achievement'>1.Achievement</h2>
-              <div id="achievement-dividor"></div>
+              <h2 id='Achievement'>Achievement</h2>
             </div>
             <h3>
             I am a passionate & hard-working student, who is interested and have experience in 
@@ -72,37 +71,38 @@ function App() {
             Web-socket application.
             </h3>
 
-          {/* Profile wrapper */}
-          <div id="profile-wrapper-2">
-            <div id="border-1-2"></div>
-            <div id="border-2-2"></div>
-            <div id="profile-2"></div>
-          </div>
-  
+            {/* Profile wrapper */}
+            <div id="profile-wrapper-2">
+              <div id="border-1-2"></div>
+              <div id="border-2-2"></div>
+              <div id="profile-2"></div>
+            </div>
         </div>
+        
+        <div className='graduation'>
+          <div id='UCL-wrapper'>
+            <div className='uni-img'>
+              <h1>University College London</h1>
+              <img className='uni-images' src={UCLBanner} alt="Could not load UCL banner"/>
+            </div>
+            <div className='uni-text'>
+              <h4>
+                Completed MSc Degree at University College London
+              </h4>
+            </div>
 
-        <div id='UCL-wrapper'>
-          <div className='uni-img'>
-            <h1>University College London</h1>
-            <img className='uni-images' src={UCLBanner} alt="Could not load UCL banner"/>
-          </div>
-          <div className='uni-text'>
-            <h4>
-              Completed MSc Degree at University College London
-            </h4>
           </div>
 
-        </div>
-
-        <div id='QMUL-wrapper'>
-          <div className='uni-text'>
-            <h4>
-              Completed BSc Degree at Queen Mary University of London.
-            </h4>
-          </div>
-          <div className='uni-img'>
-            <h1>University College London</h1>
-            <img className='uni-images' src={QueenMaryBanner} alt="Could not load Queen Mary banner"/>
+          <div id='QMUL-wrapper'>
+            <div className='uni-text'>
+              <h4>
+                Completed BSc Degree at Queen Mary University of London.
+              </h4>
+            </div>
+            <div className='uni-img'>
+              <h1>Queen Mary University of London</h1>
+              <img className='uni-images' src={QueenMaryBanner} alt="Could not load Queen Mary banner"/>
+            </div>
           </div>
         </div>
 
@@ -110,10 +110,8 @@ function App() {
 
       {/* Tab 3 */}
       <div className='tabs' id="Tab3">
-          <div id="Skills">
-            <div id="skill-dividor1"></div>
-            <h2 id='Skill-text'>2.Skills</h2>
-            <div id="skill-dividor2"></div>
+          <div id='Skills'>
+            <h2 id='Skill-text'>MY.Skills</h2>
           </div>
           <div id="skill-boxes">
             <Skillbox list = {language} svg = {LanguageIcon} title="Languages"/>
@@ -125,27 +123,79 @@ function App() {
       {/* Tab 4 */}
       <div className='tabs' id="Tab4">
         <div id="project-splash">
-          <h1 id='Project-text'>PROJECTS</h1>
+          {/* <h1 id='Project-text'>PROJECTS</h1> */}
         </div>
         <div id='project-box'>
-          {projects.map((project,key)=>{
-            if((key%2) == 0){
-              return (
-                <div className='project left-project'>
-                  {/* <img src={spaceship} alt="Value not found"/> */}
+          {
+            projects.map((project, key)=>{
+              
+              if(key%2==0){
+
+                return (
+                <div className='project ship-right'>
+                  <div className='summary-wrapper'>
+                    <div className='project-shuttle-wrapper'>
+                      <img className='project-shuttle' src={Shuttle}/>
+                    </div>
+                    <div className='summary-text-wrapper'>
+                      <h1 className='project-heading'>{project.name}</h1>
+                      <h1 className='project-subheading'>{project.subheading}</h1>
+                      <p className='project-summary'>{project.summary}</p>
+                      <div className='tools'>
+                        {
+                          project.tools.map((tool,key)=>{
+                            return (<p className='tool'>{tool.name}</p>)
+                          })
+                        }
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='project-initials-wrapper'>
+                    <h1>{project.banner}</h1>
+                  </div>
                 </div>
-              )
-            }
-            else{
-              return (
-                <div className='project right-project'>
-                  {/* <img src={spaceship} alt="Value not found"/> */}
-                </div>
-              )
-            }
-          })}
+                )
+
+              }
+              else{
+                return (
+                  <div className='project ship-left'>
+                      <div className='project-initials-wrapper'>
+                        <h1>{project.banner}</h1>
+                      </div>
+                      <div className='summary-wrapper'>
+                        
+                        <div className='summary-text-wrapper'>
+                          <h1 className='project-heading'>{project.name}</h1>
+                          <h1 className='project-subheading'>{project.subheading}</h1>
+                          <p className='project-summary'>{project.summary}</p>
+                          <div className='tools'>
+                            {
+                              project.tools.map((tool,key)=>{
+                                return (<p className='tool'>{tool.name}</p>)
+                              })
+                            }
+
+                          </div>
+                        </div>
+
+                        <div className='project-shuttle-wrapper'>
+                          <img className='project-shuttle' src={Shuttle}/>
+                        </div>
+                      </div>
+                    
+                  </div>
+                )
+
+              }
+            })
+          }
         </div>
+
       </div>
+
     </div>
   );
 }
--- src/Information source/Projects.js
+++ src/Information source/Projects.js
@@ -25,7 +25,11 @@ const tools =
         color:''
     },
     {
-        name:'Bussiness-to-Cosumber Azure Active Directory',
+        name:'B2C AAD',
+        color:''
+    },
+    {
+        name:'Selenium',
         color:''
     }
 ]
@@ -34,27 +38,35 @@ const projects =
 [
     {
         name:'Arnote',
-        color:'',
-        banner:'',
-        summary:'',
-        screenshots:[],
-        tools:[tools[0],tools[1],tools[2],tools[3]]
+        banner:'ANT',
+        summary:'A social platform, where artist can create live drawing sessions for themselves and friends, to join and draw together in real time.',
+        subheading:'Art and Socialization',
+        member:'Individual',
+        tools:[tools[0],tools[1],tools[2],tools[3],tools[4],tools[7]]
+    },
+    {
+        name:'Multi-tenant Microsoft Learn LTI',
+        banner:'LTI',
+        summary:'Improve Microsoft Learn LTI to allow universities to share the same content via different Learning Management System',
+        subheading:'Sharing Educational Resources',
+        member:'6',
+        tools:[tools[0],tools[5],tools[6],tools[7]]
     },
     {
         name:'Performance AI',
-        color:'',
-        banner:'',
-        summary:'',
-        screenshots:[],
+        banner:'PAI',
+        summary:'Improve Microsoft Performance AI app to help prepare visually impaired people to compete fairly in bicycle races. The app uses AI to suitable cycling exercise plans and intelligently guide the user through these exercise with audio signal.',
+        subheading:'Fairness for the visually impaired',
+        member:'6',
         tools:[tools[4]]
     },
     {
-        name:'Microsoft Learn LTI',
-        color:'',
-        banner:'',
-        summary:'',
-        screenshots:[],
-        tools:[tools[5],tools[6]]
+        name:'FakeNews',
+        banner:'FNW',
+        summary:'A newspaper website that allows author to create and post articles and blogs. Users can sign up and interact with the posts such as liking, commenting on the posts.',
+        subheading:'Bringing fakest news to everyone',
+        member:'3',
+        tools:[tools[3],tools[7]]
     }
 
 ]
